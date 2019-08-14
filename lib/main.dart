caf1191411f918686a5d6904428b266d7079d7fb
Alex Felipe <alexfelipevieira@gmail.com>
2019-08-14 20:03:25 -0300

    formulário para CPF

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(TelaInicial());
 
@@ -11,18 +14,48 @@ class TelaInicial extends StatelessWidget {
   }
 }
 
-class CpfFormulario extends StatelessWidget {
+class CpfFormulario extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return CpfFormularioState();
+  }
+}
+
+class CpfFormularioState extends State<CpfFormulario> {
+  final _formController = TextEditingController();
+
+  @override
+  void dispose() {
+    _formController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Preencher formulário"),
       ),
-      body: Column(
-        children: <Widget>[
-          Text("Informe o CPF"),
-          Text("dasdasda"),
-        ],
+      body: Form(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                  keyboardType: TextInputType.number,
+                  style: TextStyle(fontSize: 30.0),
+                  decoration: InputDecoration(
+                      border: InputBorder.none, hintText: 'Insira o CPF'),
+                  controller: _formController),
+            ),
+            RaisedButton(
+              child: Text("Próximo"),
+              onPressed: () {
+                print(_formController.value);
+              },
+            )
+          ],
+        ),
       ),
     );
   }
